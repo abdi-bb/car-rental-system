69f547ae6cb72d85400f3f8c54acb8dadff96551
Abdi Berhe <abdiberhe@gmail.com>
2023-12-25 23:53:59 +0300

    fix: Set the expiry date of jwt token to 30 days

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -169,7 +169,7 @@ DJOSER = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=2)
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=30)
 }
  
 # 📁 webappexample/settings.py -----
