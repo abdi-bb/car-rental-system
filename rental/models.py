5870d3c88c002af88b8de9abf9c05c3c8275f518
Abdi Berhe <abdiberhe@gmail.com>
2023-12-25 11:10:51 +0300

    feat: Add clean method to Booking class to avoid duplicate booking of a single car

--- rental/models.py
+++ rental/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -15,7 +16,7 @@ class Car(models.Model):
     seat = models.PositiveIntegerField()
     door = models.PositiveIntegerField()
     gearbox = models.CharField(max_length=1, choices=GEARBOX_CHOICES)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Customer(models.Model):
@@ -27,6 +28,20 @@ class Booking(models.Model):
     end_date = models.DateField()
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     car = models.OneToOneField(Car, on_delete=models.PROTECT)
+    
+    def clean(self):
+        # Check for overlapping bookings for the same car
+        overlapping_bookings = Booking.objects.filter(
+            car=self.car,
+            end_date__gte=self.start_date,
+            start_date__lte=self.end_date
+        ).exclude(pk=self.pk)  # Exclude the current booking if it's an update
+        if overlapping_bookings.exists():
+            raise ValidationError("This car is already booked for the specified time period.")
+
+    def save(self, *args, **kwargs):
+        self.clean()  # Perform the validation before saving
+        super().save(*args, **kwargs)
     
 class Review(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='reviews')
