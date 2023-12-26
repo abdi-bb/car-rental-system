a8fab05f487aab983280b18b47d0006666ca912e
Abdi Berhe <abdiberhe@gmail.com>
2023-12-27 02:11:36 +0300

    feat: Create a new model 'CarImage'

--- rental/models.py
+++ rental/models.py
@@ -16,11 +16,17 @@ class Car(models.Model):
     seat = models.PositiveIntegerField()
     door = models.PositiveIntegerField()
     gearbox = models.CharField(max_length=1, choices=GEARBOX_CHOICES)
-    image = models.ImageField(null=True, blank=True)
+    # image = models.ImageField(upload_to='rental/cars/images', null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     
     def __str__(self):
         return f"{self.name} ({self.model})"
+    
+    
+class CarImage(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='rental/cars/images', null=True, blank=True)
+
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
