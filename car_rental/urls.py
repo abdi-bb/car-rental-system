893daa8f73f16bc334c6bb66e72abed1b5788341
Abdi Berhe <abdiberhe@gmail.com>
2023-12-27 01:29:25 +0300

    feat: Add url for image upload

--- car_rental/urls.py
+++ car_rental/urls.py
@@ -27,3 +27,9 @@ urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
     
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
