3162dd7292baae48da8c08ea671683d3d01f21ab
Abdi Berhe <abdiberhe@gmail.com>
2023-12-31 01:27:10 +0300

    fix: Arrange all server side codes inside a separate folder backend

--- customer/api/v1/signals.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from customer.models import Customer
-
-@receiver(post_save, sender=User)
-def create_customer_for_new_user(sender, **kwargs):
-    if kwargs['created']:
-        Customer.objects.create(user=kwargs['instance'])
--- review/api/v1/filters.py
+++
@@ -1,1 +0,0 @@
-from django_filters.rest_framework import FilterSet
