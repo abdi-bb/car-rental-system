9a87ee8177041520a5f163c6bae3c8c1d98b6db4
Abdi Berhe <abdiberhe@gmail.com>
2023-12-31 21:12:39 +0300

    fix: Add 'rating' field to the serializer

--- backend/review/api/v1/serializers.py
+++ backend/review/api/v1/serializers.py
@@ -7,7 +7,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     username = serializers.StringRelatedField(source='customer.user.username', read_only=True)
     class Meta:
         model = Review
-        fields = ['id', 'customer_id', 'username', 'description', 'created_at', 'updated_at']
+        fields = ['id', 'customer_id', 'username', 'description', 'created_at', 'updated_at', 'rating']
         
     def create(self, validated_data):
         car_id = self.context['car_id']
