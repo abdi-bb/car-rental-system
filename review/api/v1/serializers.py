06dfb696d7b0392cf00d199f8f0b4889085d0260
Abdi Berhe <abdiberhe@gmail.com>
2024-01-05 21:31:20 +0300

    fix: Correct some visible fields for frontend

--- review/api/v1/serializers.py
+++ review/api/v1/serializers.py
@@ -1,16 +1,14 @@
 from rest_framework import serializers
 
 from review.models import Review
-from car.api.v1.serializers import CarImageSerializer
     
     
 class ReviewSerializer(serializers.ModelSerializer):
     username = serializers.StringRelatedField(source='user.username', read_only=True)
     car_name = serializers.StringRelatedField(source='car.name', read_only=True)
-    car_images = CarImageSerializer(source='car.images', many=True, read_only=True)
     class Meta:
         model = Review
-        fields = ['id', 'user_id', 'username', 'car_id', 'car_name', 'car_images', 'description', 'created_at', 'updated_at', 'rating']
+        fields = ['id', 'user_id', 'username', 'car_id', 'car_name', 'description', 'created_at', 'updated_at', 'rating']
         
     def create(self, validated_data):
         car_id = self.context['car_id']
