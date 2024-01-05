b65957b3a247efb8a51eefa0b4ba60783cdfed3e
Abdi Berhe <abdiberhe@gmail.com>
2024-01-05 16:53:50 +0300

    fix: Update fields to be displayed in the serializer

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -194,7 +194,7 @@ DJOSER = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-   'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
 #    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
 }
  
